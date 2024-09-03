e8c516ce3d4a1323238b8ee3b0a8b88e97475012
Yasuaki Tamura <y-tamura>
2024-09-03 16:05:02 +0900

    fix:  generate incorrect hash value of public key in iOS

--- ios/Classes/SwiftHttpCertificatePinningPlugin.swift
+++ ios/Classes/SwiftHttpCertificatePinningPlugin.swift
@@ -1,7 +1,6 @@
 import Flutter
-import UIKit
-import CryptoSwift
 import Alamofire
+import CommonCrypto
 
 public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
 
@@ -92,7 +91,7 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
         }
 
         manager.delegate.sessionDidReceiveChallenge = { session, challenge in
-            guard let serverTrust = challenge.protectionSpace.serverTrust, let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
+            guard let serverTrust = challenge.protectionSpace.serverTrust else {
                 flutterResult(
                     FlutterError(
                         code: "ERROR CERT",
@@ -100,64 +99,115 @@ public class SwiftHttpCertificatePinningPlugin: NSObject, FlutterPlugin {
                         details: nil
                     )
                 )
-                
                 return (.cancelAuthenticationChallenge, nil)
             }
-
+            
             // Set SSL policies for domain name check
-            let policies: [SecPolicy] = [SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString))]
-            SecTrustSetPolicies(serverTrust, policies as CFTypeRef)
-
-            // Evaluate server certificate
-            var result: SecTrustResultType = .invalid
-            SecTrustEvaluate(serverTrust, &result)
-            let isServerTrusted: Bool = (result == .unspecified || result == .proceed)
-
-            guard let publicKey = SecCertificateCopyKey(certificate),
-                  let publicKeyData = SecKeyCopyExternalRepresentation(publicKey, nil) as Data? else {
-                flutterResult(
-                    FlutterError(
-                        code: "ERROR CERT",
-                        message: "Invalid Public Key",
-                        details: nil
+            let policies = NSMutableArray()
+            policies.add(SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString)))
+            SecTrustSetPolicies(serverTrust, policies)
+            
+            let isServerTrusted = SecTrustEvaluateWithError(serverTrust, nil)
+            if(isServerTrusted) {
+                guard let certificates = SecTrustCopyCertificateChain(serverTrust) as? [SecCertificate] else {
+                    flutterResult(
+                        FlutterError(
+                            code: "ERROR CERT",
+                            message: "Invalid Certificate",
+                            details: nil
+                        )
                     )
-                )
-                return (.cancelAuthenticationChallenge, nil)
-            }
 
-            var publicKeySha = publicKeyData.sha256().toHexString()
+                    return (.cancelAuthenticationChallenge, nil)
+                }
+                
+                // For certificate Key
+                //let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0)
+                // let remoteCertData = SecCertificateCopyData(serverCertificate) as Data
+                // var certificateKey = remoteCertData.sha256().toHexString()
+                
+                /*
+                let publicKeyList = Set( certificates.map { SecCertificateCopyKey($0)  } )
+                publicKeyList.forEach { secKey in
+                    if let secKey = secKey, let publicKeyData = SecKeyCopyExternalRepresentation(secKey, nil) as? Data {
+                        let keyString = self.sha256(data: publicKeyData)
+                        print(keyString)
+                    }
+                }
+                 */
+                
+                var isSecure = false
+                
+                if let secKey = SecCertificateCopyKey(certificates[0]) , let publicKeyData = SecKeyCopyExternalRepresentation(secKey, nil) as? Data {
+                    let keyString = self.sha256(data: publicKeyData)
+                    print(keyString)
 
-            if(type == "SHA1"){
-                publicKeySha = publicKeyData.sha1().toHexString()
-            }
-
-
-            var isSecure = false
-            if var fp = self.fingerprints {
-                fp = fp.compactMap { (val) -> String? in
-                    val.replacingOccurrences(of: " ", with: "")
-            }
-
-                isSecure = fp.contains(where: { (value) -> Bool in
-                    value.caseInsensitiveCompare(publicKeySha) == .orderedSame
-                })
-            }
-
-            if isServerTrusted && isSecure {
-                flutterResult("CONNECTION_SECURE")
-                resultDispatched = true
-            } else {
-                flutterResult(
-                    FlutterError(
-                        code: "CONNECTION_NOT_SECURE",
-                        message: nil,
-                        details: nil
+                    if var fp = self.fingerprints {
+                        fp = fp.compactMap { (val) -> String? in
+                            val.replacingOccurrences(of: " ", with: "")
+                        }
+                        isSecure = fp.contains(where: { (value) -> Bool in
+                            value.caseInsensitiveCompare(keyString) == .orderedSame
+                        })
+                    }
+                }
+                else {
+                    flutterResult(
+                        FlutterError(
+                            code: "ERROR CERT",
+                            message: "Invalid Public Key",
+                            details: nil
+                        )
                     )
-                )
-                resultDispatched = true
-            }
+                    return (.cancelAuthenticationChallenge, nil)
+                }
+                
+                if isServerTrusted && isSecure {
+                    flutterResult("CONNECTION_SECURE")
+                    resultDispatched = true
+                } else {
+                    flutterResult(
+                        FlutterError(
+                            code: "CONNECTION_NOT_SECURE",
+                            message: nil,
+                            details: nil
+                        )
+                    )
+                    resultDispatched = true
+                }
 
+            }
+            
             return (.cancelAuthenticationChallenge, nil)
         }
     }
+    
+    private func sha256(data : Data) -> String {
+        let rsa2048Asn1Header:[UInt8] = [
+            0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
+            0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0f, 0x00
+        ]
+
+        var keyWithHeader = Data(rsa2048Asn1Header)
+        keyWithHeader.append(data)
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+        keyWithHeader.withUnsafeBytes {
+            // _ = CC_SHA256($0, CC_LONG(keyWithHeader.count), &hash) // deprecated withUnsafeBytes
+            guard let pointer = $0.baseAddress?.assumingMemoryBound(to: UInt8.self) else { return }
+            _ = CC_SHA256(pointer, CC_LONG(keyWithHeader.count), &hash)
+        }
+//        return Data(hash).base64EncodedString()
+        return Data(hash).toHexString()
+
+        }
+
 }
+
+extension Data {
+     func toHexString() -> String {
+        return String(self.flatMap { byte in
+            String(format:"%02x", byte)
+        })
+    }
+}
+
